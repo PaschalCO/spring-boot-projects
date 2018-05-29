2326a497f82c3130fe941184929cf2edf0ecda92
p.chukwudum <Michaelmyidol.123>
2018-05-29 11:30:46 +0200

    First commit

---
+++ src/main/java/com/barcelo/microservices/productappsmasterdata/ProductAppsMasterdataApplication.java
@@ -0,0 +1,12 @@
+package com.barcelo.microservices.productappsmasterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAppsMasterdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductAppsMasterdataApplication.class, args);
+	}
+}
---
+++ src/test/java/com/barcelo/microservices/productappsmasterdata/ProductAppsMasterdataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.barcelo.microservices.productappsmasterdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductAppsMasterdataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
